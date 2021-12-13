50260a6c1dd6bb47c6817ce2dac249ad690a8fc8
Nastya Chichagova <91213713+nastyachichagova@users.noreply.github.com>
2021-12-13 16:47:30 +0300

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -12,44 +12,55 @@ public class Lab4 {
         int A[], N;
 
         System.out.print("Введите размерность массива: ");
-        N = in.nextInt();
 
-        A = new int[N];
+        if(in.hasNextInt()){
+            N = in.nextInt();
 
-        for (int i = 0; i < N; i++){
-            System.out.print("Введите " + (i+1) + "-й элемент массива: ");
-            A[i] = in.nextInt();
-        }
+            A = new int[N];
 
-        System.out.print("Введите число X: ");
-        int X = in.nextInt();
-
-        System.out.print("Оригинальный массив: ");
-        for (int i = 0; i < A.length; i++) {
-            System.out.print(A[i] + " ");
-        }
-        System.out.println();
-
-        int k = 0;
-
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == X){
-                k += 1;}
-        }
-
-        int[] newA = new int [A.length-k];
-        for (int i = 0, j = 0; i < A.length; i++) {
-            if (A[i] != X) {
-                newA[j++] = A[i];
+            for (int i = 0; i < N; i++){
+                System.out.print("Введите " + (i+1) + "-й элемент массива: ");
+                A[i] = in.nextInt();
             }
-        }
 
-        System.out.print("Преобразованный массив: ");
-        for (int i = 0; i < newA.length; i++) {
-            System.out.print(newA[i] + " ");
-        }
-        System.out.println();
+            System.out.print("Введите число X: ");
 
-        in.close();
+            if(in.hasNextInt()) {
+                int X = in.nextInt();
+
+                System.out.print("Оригинальный массив: ");
+                for (int i = 0; i < A.length; i++) {
+                    System.out.print(A[i] + " ");
+                }
+                System.out.println();
+
+                int k = 0;
+
+                for (int i = 0; i < A.length; i++) {
+                    if (A[i] == X) {
+                        k += 1;
+                    }
+                }
+
+                int[] newA = new int[A.length - k];
+                for (int i = 0, j = 0; i < A.length; i++) {
+                    if (A[i] != X) {
+                        newA[j++] = A[i];
+                    }
+                }
+
+                System.out.print("Преобразованный массив: ");
+                for (int i = 0; i < newA.length; i++) {
+                    System.out.print(newA[i] + " ");
+                }
+                System.out.println();
+
+                in.close();
+            }
+            else
+                System.out.println("Введены неверные данные");
+        }
+        else
+            System.out.println("Введены неверные данные");
     }
 }
